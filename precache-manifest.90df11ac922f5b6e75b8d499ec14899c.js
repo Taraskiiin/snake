3e16cdd5c598188f4b5c08c810781b19b2fef8ca
taraskiiin <taraskravets95@gmail.com>
2020-04-17 21:51:54 +0300

    Updates

---
+++ precache-manifest.90df11ac922f5b6e75b8d499ec14899c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b945821393dbde41fd134762e06a573",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "71074628c71bbb2115a0",
+    "url": "/snake/static/css/main.d0c951e1.chunk.css"
+  },
+  {
+    "revision": "5928405a567a2a370eca",
+    "url": "/snake/static/js/2.f7d4ba49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.f7d4ba49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71074628c71bbb2115a0",
+    "url": "/snake/static/js/main.64797b74.chunk.js"
+  },
+  {
+    "revision": "5b61f01a715615e2dc1a",
+    "url": "/snake/static/js/runtime-main.cc1a9ef3.js"
+  },
+  {
+    "revision": "5ec0a58bfab3f604cd1da37967b53e2c",
+    "url": "/snake/static/media/snakeman.5ec0a58b.png"
+  }
+]);
